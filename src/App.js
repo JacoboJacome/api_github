dc2f4fbe125d60af9a37a716caa266e98a679cd6
JacoboJacome <jacobojacome2@gmail.com>
2021-11-20 18:42:23 -0500

    ruta

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <statusUserProvider>
         <MainLayout>
         <Switch>
-            <Route path="/" exact>
+            <Route path="http://jacobojacome.github.io/api_github" exact>
               <Home />
             </Route>
             <Route path="/:user/followers" exact>
